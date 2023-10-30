5ea34de0769a98caa64b269e01575393af741e2b
MonaDhote <ingole.pratiksha166@gmail.com>
2023-10-30 22:00:15 +0530

    main code

---
+++ src/main/java/demotogit/copy/demo.java
@@ -0,0 +1,12 @@
+package demotogit.copy;
+
+public class demotogit {
+	int master;
+	double pratiksha;
+	float vijaya;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
